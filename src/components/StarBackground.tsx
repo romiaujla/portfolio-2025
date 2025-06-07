e3c0c90c6a17d7554aa9caa9f8367a0a17d59b9c
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-06-07 09:39:48 -0400

    [PORTFOLIO-2025] Updated the styling on the meteor to look sharper

--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState, type JSX } from "react";
 
-type StarModel = {
+type BackgroundPropsModel = {
     id: number;
     x: number;
     y: number;
     size: number;
-    opacity: number;
     animationDuration?: number;
 };
 
+type StarModel = BackgroundPropsModel & {
+    opacity: number;
+};
+
+type MeteorModel = BackgroundPropsModel & {
+    delay: number
+}
+
 export const StarBackground = () => {
-    const [stars, setStars] = useState<StarModel[]>([]);
+    const [stars, setStars] = useState<Array<StarModel>>([]);
+    const [meteors, setMeteors] = useState<Array<MeteorModel>>([]);
 
-    const generateStars = () => {
-        const newStars = [];
+    useEffect(() => {
+        generateStars();
+        generateMeteors();
+        handleResize();
 
-        const numberOfStars = Math.floor(
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const generateStars = (): void => {
+        const newStars: Array<StarModel> = [];
+
+        const numberOfStars: number = Math.floor(
             window.innerWidth * window.innerHeight / 10000
         )
 
@@ -26,16 +46,73 @@ export const StarBackground = () => {
                 y: Math.random() * 100,
                 size: Math.random() * 3 + 1,
                 opacity: Math.random() * 0.5 + 0.5,
-                animationDuration: Math.random() * 4 + 2
+                animationDuration: Math.random() * 4 + 2,
             })
         }
 
         setStars(newStars);
     }
 
-    return (
-        <div className="">
+    const generateMeteors = (): void => {
+        const newMeteors: Array<MeteorModel> = [];
+        const numberOfMeteors: number = 4;
 
+        for (let i = 0; i < numberOfMeteors; i++) {
+            newMeteors.push({
+                id: i,
+                x: Math.random() * 100,
+                y: Math.random() * 20,
+                size: Math.random() * 2 + 1,
+                delay: Math.random() * 15,
+                animationDuration: Math.random() * 3 + 3,
+            })
+        }
+
+        setMeteors(newMeteors);
+    }
+
+    const handleResize = (): void => {
+        generateStars();
+        generateMeteors();
+    }
+
+
+
+    return (
+        <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+            {
+                stars.map((star: StarModel): JSX.Element => {
+                    return <div
+                        key={star.id}
+                        className="star animate-pulse-subtle"
+                        style={{
+                            width: `${star.size}px`,
+                            height: `${star.size}px`,
+                            left: `${star.x}%`,
+                            top: `${star.y}%`,
+                            opacity: star.opacity,
+                            animationDuration: `${star.animationDuration}s`,
+                        }}
+                    />
+                })
+            }
+
+            {
+                meteors.map((meteor: MeteorModel): JSX.Element => {
+                    console.log('Size', meteor.size)
+                    return <div
+                        key={meteor.id}
+                        className="meteor animate-meteor"
+                        style={{
+                            width: `${meteor.size * 30}px`,
+                            height: `${meteor.size / 2}px`,
+                            left: `${meteor.x}%`,
+                            top: `${meteor.y}%`,
+                            animationDuration: `${meteor.animationDuration}s`,
+                        }}
+                    />
+                })
+            }
         </div>
     );
 }
